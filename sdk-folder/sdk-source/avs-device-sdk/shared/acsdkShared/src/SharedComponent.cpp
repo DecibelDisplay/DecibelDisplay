6b047f9d40d053429c4f6aa977d2ddba485e6f6c
Smart Music Display <chaley@hawk.iit.edu>
2021-03-13 11:21:10 -0600

    Add back avs-device-sdk

---
+++ sdk-folder/sdk-source/avs-device-sdk/shared/acsdkShared/src/SharedComponent.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <acsdkManufactory/ComponentAccumulator.h>
+#include <acsdkManufactory/ConstructorAdapter.h>
+#include <acsdkNotifier/Notifier.h>
+#include <acsdkShutdownManager/ShutdownManager.h>
+#include <acsdkShutdownManager/ShutdownNotifier.h>
+#include <acsdkStartupManager/StartupManager.h>
+#include <acsdkStartupManager/StartupNotifier.h>
+#include <AVSCommon/Utils/LibcurlUtils/HttpPost.h>
+
+#include "acsdkShared/SharedComponent.h"
+
+namespace alexaClientSDK {
+namespace acsdkShared {
+
+using namespace acsdkManufactory;
+using namespace acsdkShutdownManager;
+using namespace acsdkShutdownManagerInterfaces;
+using namespace acsdkStartupManager;
+using namespace acsdkStartupManagerInterfaces;
+using namespace avsCommon;
+using namespace avsCommon::utils;
+using namespace avsCommon::utils::configuration;
+using namespace avsCommon::utils::libcurlUtils;
+using namespace avsCommon::utils::timing;
+
+SharedComponent getComponent() {
+    return ComponentAccumulator<>()
+        .addRetainedFactory(ConfigurationNode::createRoot)
+        .addRetainedFactory(ConstructorAdapter<MultiTimer>::get())
+        .addUniqueFactory(HttpPost::createHttpPostInterface)
+        .addRetainedFactory(ShutdownManager::createShutdownManagerInterface)
+        .addRetainedFactory(ConstructorAdapter<ShutdownNotifierInterface, ShutdownNotifier>::get())
+        .addRetainedFactory(StartupManager::createStartupManagerInterface)
+        .addRetainedFactory(ConstructorAdapter<StartupNotifierInterface, StartupNotifier>::get())
+        .addRetainedFactory(SystemClockMonitor::createSystemClockMonitor);
+}
+
+}  // namespace acsdkShared
+}  // namespace alexaClientSDK
