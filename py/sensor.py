fc1904014a1e2dbcacf543bb9e971eabc0c34676
kkaczmarczyk116 <77896639+kkaczmarczyk116@users.noreply.github.com>
2021-04-22 13:31:15 -0500

    Create sensor.py

---
+++ py/sensor.py
@@ -0,0 +1,12 @@
+import time
+from smbus import SMBus
+from bme280 import BME280
+bus= SMBus(1)
+bme280 =BME280(i2c_dev = bus)
+
+while True:
+    temp = bme280.get_temperature()
+    pres = bme280.get_pressure()
+    hum = bme280.get_humidity()
+    print(str(round(temp,4)) + " C " + str(round(pres,4)) + " hPA" + str(round(hum,4)) + " %" )
+    sleep(5)
